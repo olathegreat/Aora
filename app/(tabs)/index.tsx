dfcfcea443419cebd3ca76539db6c88a94c9c38f
olathegreat <ademolarotimi2000@gmail.com>
2025-03-16 17:28:46 +0100

    fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,13 +4,12 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Link } from 'expo-router'
 import { StyleSheet } from 'react-native'
-// import "./../../global.css"
-// import "../global.css"
+
 
 // const StyledView = styled(View)
 const index = () => {
   return (
-    <View className='w-80 h-80  bg-red-500' style={styles.container}>
+    <View className='w-80 h-80  !bg-red-500' style={styles.container}>
       <Text className="text-red-500">Aora</Text>
       <StatusBar style="auto"/>
       <Link href="/profile" style={{color:'blue'}}>Go to profile</Link>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,7 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import "../global.css"
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
